dd376b6cd546822b827fff3fc4b00662b726d48b
Bree Thomas <bree@aha-method.com>
2013-11-27 21:30:49 -0700

    WIP: refactor first solution

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -1,19 +1,23 @@
 class Year
+  attr_reader :year
 
   def initialize(year)
     @year = year
   end
 
-  # if divisible by 4, then leap candidate
-  # if leap candidate, divide by 100.
-  # if evenly divisible by 100, then divide by 400
-  # if evenly divisible by 400 then leap year true
+  def leap_candidate?
+    year % 4 == 0
+  end
+
+  def simple_leap?
+    leap_candidate? && year % 100 != 0
+  end
 
   def leap?
-    if @year % 4 == 0 && @year % 100 != 0
+    if simple_leap? 
       true
     elsif
-      @year % 4 == 0 && @year % 100 == 0 && @year % 400 == 0
+      year % 4 == 0 && year % 100 == 0 && year % 400 == 0
       true
     else
       false
