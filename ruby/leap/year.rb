a8ac63c715288d9603f454d6860b940a1497a320
Bree Thomas <bree@aha-method.com>
2013-11-30 22:12:37 -0700

    WIP: extract methods for leap-year computations

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -6,6 +6,18 @@ class Year
   end
 
   def leap?
-    year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+    leap_candidate? && valid_century? || exceptional_century?
+  end
+
+  def leap_candidate?
+    year % 4 == 0
+  end
+
+  def valid_century?
+    year % 100 != 0
+  end
+
+  def exceptional_century?
+    year % 400 == 0
   end
 end
