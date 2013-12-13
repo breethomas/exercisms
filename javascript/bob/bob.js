27f7db4d6a6213c98cd6306622f0882f4ec56e24
Bree Thomas <bree@aha-method.com>
2013-12-13 15:37:45 -0700

    Round one solution

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,15 +1,28 @@
-var Bob = function() {
-  this.hey = function(input) {
-    if(input.search(/[a-zA-Z]/) !== -1 && input.toUpperCase() === input) {
-      return "Woah, chill out!";
-    } else if(input[input.length - 1] === "?") {
-      return "Sure.";
-    } else if(input.search(/[\s+]/) === -1 || input.search(/[\s++]/) === -1) {
-      return "Fine. Be that way!"
-    } else {
-      return "Whatever.";
-    }
-  }
+function Bob() {
 }
 
-exports.Bob = Bob;
+Bob.prototype.hey = function(input) {
+  if (isShouting(input)) {
+    return "Woah, chill out!";
+  } else if(isQuestioning(input)) {
+    return "Sure.";
+  } else if(isSilence(input)) {
+    return "Fine. Be that way!";
+  } else {
+    return "Whatever.";
+  }
+};
+
+function isShouting(input) {
+  return (input.toUpperCase() === input && /[A-Z]/.test(input));
+}
+
+function isQuestioning(input) {
+  return (input[input.length - 1] === "?");
+}
+
+function isSilence(input) {
+  return (input.replace(/\s+/g, '')) === '';
+}
+
+module.exports = Bob;
