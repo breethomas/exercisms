10c19daf5d7b1777fb4e2c8e4c1e6eacfc9c1776
Bree Thomas <bree@aha-method.com>
2013-12-26 11:23:57 -0700

    Proverb iteration 7

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -13,18 +13,16 @@ class Proverb
   attr_reader :consequences, :modifier
 
   def chorus
-    chorus =  consequence_pairs.collect do |old_thing, new_thing|
+    consequences.each_cons(2).collect do |old_thing, new_thing|
       "For want of a #{old_thing} the #{new_thing} was lost.\n"
-    end
-    chorus.join
+    end.join
   end
 
   def last_line
-    original_thing = [modifier, consequences.first].compact
-    "And all for the want of a #{original_thing.join(' ')}."
+    "And all for the want of a #{adjective.join(' ')}."
   end
 
-  def consequence_pairs
-    consequences.each_cons(2).collect {|pair| pair}
+  def adjective
+    [modifier, consequences.first].compact
   end
 end
