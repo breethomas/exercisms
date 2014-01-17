29153f697bf83f74a93af881234e3b7877b97e9c
Bree Thomas <bree@aha-method.com>
2014-01-16 19:36:59 -0700

    Refactor years method to remove redundant code per planet

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -6,60 +6,52 @@ class SpaceAge
   end
 
   def on_earth
-    planet_orbital = orbital_periods[:earth]
-    years(planet_orbital)
+    years_on(:earth)
   end
 
   def on_mercury
-    planet_orbital = orbital_periods[:mercury]
-    years(planet_orbital)
+    years_on(:mercury)
   end
 
   def on_venus
-    planet_orbital = orbital_periods[:venus]
-    years(planet_orbital)
+    years_on(:venus)
   end
 
   def on_mars
-    planet_orbital = orbital_periods[:mars]
-    years(planet_orbital)
+    years_on(:mars)
   end
 
   def on_jupiter
-    planet_orbital = orbital_periods[:jupiter]
-    years(planet_orbital)
+    years_on(:jupiter)
   end
 
   def on_saturn
-    planet_orbital = orbital_periods[:saturn]
-    years(planet_orbital)
+    years_on(:saturn)
   end
 
   def on_uranus
-    planet_orbital = orbital_periods[:uranus]
-    years(planet_orbital)
+    years_on(:uranus)
   end
 
   def on_neptune
-    planet_orbital = orbital_periods[:neptune]
-    years(planet_orbital)
+    years_on(:neptune)
   end
 
   private
 
   attr_reader :orbital_periods
 
-  def earth_seconds
-    31557600.to_f
-  end
-
-  def years(planet_orbital)
+  def years_on(planet)
+    planet_orbital = orbital_periods[planet]
     planet_seconds = planet_orbital * earth_seconds
     (seconds/planet_seconds).round(2)
   end
 
+  def earth_seconds
+    31557600.to_f
+  end
+
   def orbital_periods
-    @orbital_periods =
       {
       earth: 1,
       mercury: 0.2408467,
