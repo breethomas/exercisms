f15ee66e5750b3e70024c3808ffe4c5bb2526e5e
Bree Thomas <bree@aha-method.com>
2014-01-17 08:57:39 -0700

    Try Ruby's define_method for similar method generation

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -3,56 +3,21 @@ class SpaceAge
 
   def initialize(input_seconds)
     @seconds = input_seconds.to_f
-  end
-
-  def on_earth
-    years_on(:earth)
-  end
-
-  def on_mercury
-    years_on(:mercury)
-  end
-
-  def on_venus
-    years_on(:venus)
-  end
-
-  def on_mars
-    years_on(:mars)
-  end
-
-  def on_jupiter
-    years_on(:jupiter)
-  end
-
-  def on_saturn
-    years_on(:saturn)
-  end
-
-  def on_uranus
-    years_on(:uranus)
-  end
-
-  def on_neptune
-    years_on(:neptune)
+    define_on_planet_methods
   end
 
   private
 
   attr_reader :orbital_periods
 
-  def years_on(planet)
-    planet_orbital = orbital_periods[planet]
-    planet_seconds = planet_orbital * earth_seconds
-    (seconds/planet_seconds).round(2)
-  end
-
-  def earth_seconds
-    31557600.to_f
+  def define_on_planet_methods
+    orbital_periods.each_key do |planet|
+    self.class.send(:define_method, "on_#{planet}") {years_on(planet)}
+    end
   end
 
   def orbital_periods
-      {
+    {
       earth: 1,
       mercury: 0.2408467,
       venus: 0.61519726,
@@ -62,5 +27,15 @@ class SpaceAge
       uranus: 84.016846,
       neptune: 164.79132
     }
+  end
+
+  def years_on(planet)
+    planet_orbital = orbital_periods[planet]
+    planet_seconds = planet_orbital * earth_seconds
+    (seconds/planet_seconds).round(2)
+  end
+
+  def earth_seconds
+    31557600.to_f
   end
 end
