059ce078afc86f0cabddfb1f229d5a5846859667
Bree Thomas <bree@aha-method.com>
2014-01-28 17:47:40 -0700

    remove class methods from space age

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,7 +1,6 @@
 class SpaceAge
   attr_reader :seconds
 
-  def self.orbital_periods
     {
       earth: 1.0,
       mercury: 0.2408467,
@@ -11,19 +10,11 @@ class SpaceAge
       saturn: 29.447498,
       uranus: 84.016846,
       neptune: 164.79132
-    }
-  end
-
-  orbital_periods.each do |planet, planet_orbital|
-    define_method("on_#{planet}") do
-      planet_seconds = planet_orbital * earth_year
-      (seconds/planet_seconds).round(2)
-    end
-  end
-
-  def earth_year
-    31_557_600.0
-  end
+    }.each {|planet, planet_orbital|
+        planet_seconds = planet_orbital * 31_557_600
+        define_method("on_#{planet}") do
+          (seconds/planet_seconds).round(2)
+        end}
 
   def initialize(input_seconds)
     @seconds = input_seconds.to_f
