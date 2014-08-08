dc6df1f526724612eebca58ccc877d89efa74b8c
Bree Thomas <bree@aha-method.com>
2014-08-08 11:38:40 -0600

    Cleanup old Bob exercise

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,6 +1,17 @@
 function Bob() {
 }
 
+function isShouting(input) {
+  return (input.toUpperCase() === input && /[A-Z\xdc]/.test(input));
+}
+
+function isQuestioning(input) {
+  return (input[input.length - 1] === "?");
+}
+
+function isSilence(input) {
+  return (input.trim() === "");
+}
 Bob.prototype.hey = function(input) {
   if (isShouting(input)) {
     return "Woah, chill out!";
@@ -13,45 +24,4 @@ Bob.prototype.hey = function(input) {
   }
 };
 
-function isShouting(input) {
-  return (input.toUpperCase() === input && /[A-Z\xdc]/.test(input));
-}
-
-function isQuestioning(input) {
-  return (input[input.length - 1] === "?");
-}
-
-function isSilence(input) {
-  return (input.trim() === "");
-}
-
 module.exports = Bob;
-
-
-//function Jane(age = 32) {
-//this.age = age
-//}
-//Jane.prototype.isGolden = function() {
-//  this.age >= 50
-//}
-//
-//
-//j = new Jane(50)
-//j.age == 50
-//j.isGolden() == true
-//
-//
-//class Jane
-//  attr_reader :age
-//  def initialize(age = 32)
-//    @age = 32
-//  end
-//
-//  def is_golden?
-//    @age >= 50
-//  end
-//end
-//
-//j = Jane.new(50)
-//j.age == 50
-//j.is_golden? == true
